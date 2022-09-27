4c5f3d85fc3261a71173251d5fed569a0b0f67b7
Evgeniy Chernavin <johnsilverne@gmail.com>
2022-09-27 19:45:44 +0600

    updated

--- scripts/janus.sh
+++ scripts/janus.sh
@@ -4,7 +4,7 @@ git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 
 sh autogen.sh
-./configure --prefix=/opt/janus
+./configure --prefix=/opt/janus --enable-websockets
 make
 make install
 make configs
